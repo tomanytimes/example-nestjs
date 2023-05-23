481ceca99420060ad405857b399df9bf4d5fd43a
이상호/(협력사) 대리/로봇관제Project(sangho98.lee@lgepartner.com) <sangho98.lee@lgepartner.com>
2023-05-23 15:22:38 +0900

    mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { myDocument, myschema } from './app.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.db_user}:${encodeURIComponent(process.env.db_password)}@atlascluster.rmnwsdf.mongodb.net/?retryWrites=true&w=majority`),
+    MongooseModule.forFeature([{name : myDocument.name, schema : myschema}])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
