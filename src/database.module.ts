65ddb989370aad1c6eeef399e3515f793c91f57e
shlee <kya100789@naver.com>
2023-06-13 04:05:38 +0900

    asd

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,16 +1,14 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// `mongodb+srv://${configService.get("db_user")}:${encodeURIComponent(configService.get("db_password"))}@atlascluster.rmnwsdf.mongodb.net/?retryWrites=true&w=majority`
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        uri: `mongodb+srv://${configService.get("db_user")}:${encodeURIComponent(
-          configService.get("db_password")
-        )}@atlascluster.rmnwsdf.mongodb.net/?retryWrites=true&w=majority`,
+        uri: configService.get('MONGODB_URI'),
       }),
     }),
   ],
